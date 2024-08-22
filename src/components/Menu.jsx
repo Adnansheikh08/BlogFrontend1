eaf4af428ac5c4097dc00f91f8396fe8933b377b
Adnan Sheik <you@example.com>
2024-08-23 00:22:39 +0530

    08-23-2024

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React ,{useState} from 'react'
+import { useNavigate  } from 'react-router-dom';
+import { BsPlusCircle, BsX } from "react-icons/bs";
 import { toast } from 'sonner';
 function Menu(props) {
-  console.log(props.id)
+  const [update, setUpdate] = useState(false)
+  const [newUpdate, setNewUpdate] = useState("")
   const navigate = useNavigate();
   function deleteblog(){
     
@@ -20,6 +22,7 @@ function Menu(props) {
        
         navigate("/");
         toast.success('Blog deleted');
+        navigate(0)
       } else {
         toast.error('Invalid Credentials');
       }
@@ -29,12 +32,75 @@ function Menu(props) {
       console.error(error);
     });
   }
+  function updateblog(){
+    fetch(`http://localhost:5050/update-post/${props.id}`,{
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            content: newUpdate,
+            }),
+            })
+            .then((res) => res.json())
+            .then(data => {
+              if(data.success)
+              {
+                setUpdate(false)
+                toast.success("Updated Successfully")
+                // navigate(0)
+              }
+              else{
+                toast.error(data.error)
+              }
+            })
+            .catch((error) => {
+              toast.error('Error updating blog'); 
+            })
+            .finally(() => {
+              navigate(0);
+            }
+            );
+
+      }
 
   return (
-    <div className="border rounded text-sm absolute bg-white right-3 top-9 shadow-2xl">
-      <button className="p-2 border-b">Edit blog</button>
-      <button  onClick={()=>deleteblog()} className="p-2">Delete blog</button>
-    </div>
+    update ? (
+      <div className="fixed inset-0 glass z-50 flex justify-center items-center ">
+        <div className="w-[400px] bg-white shadow-lg rounded-md">
+          <div className="font-bold flex justify-between items-center p-3 border-b">
+            Update your Blog
+            <BsX
+              className="text-xl cursor-pointer"
+              
+            />
+          </div>
+          <div className="p-4">
+            <textarea
+              value={newUpdate}
+              onChange={(e) => setNewUpdate(e.target.value)}
+              name=""
+              id=""
+              cols="30"
+              rows="5"
+              className="w-full border rounded mt-4 p-2"
+              placeholder="Update your blog here"
+            ></textarea>
+           <button onClick={()=>updateblog()} className="bg-black rounded-md mt-4 text-white px-3 py-1 text-sm">
+Update blog
+</button>
+           <button onClick={()=>setUpdate(false)}  className="bg-black rounded-md mt-4 ml-2 text-white px-3 py-1 text-sm">
+Cancle
+</button>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div className="border rounded text-sm absolute bg-white right-3 top-9 shadow-2xl">
+        <button onClick={()=>setUpdate(true)} className="p-2 border-b">Edit blog</button>
+        <button onClick={deleteblog} className="p-2">Delete blog</button>
+      </div>
+    )
   );
 }
 
